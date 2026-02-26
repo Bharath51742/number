0112fab2a54386f49a733751aa57dac856a5e13f
bharath <bharathofficial133@gmail.com>
2026-02-26 17:50:30 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // create Scanner inside main
+
+        System.out.println("enter a value");
+        int a = sc.nextInt();
+
+        System.out.println("enter b value");
+        int b = sc.nextInt();
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        sc.close();
+    }
+}
