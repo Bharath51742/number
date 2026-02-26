cb374eb63d948766ed1467eef0aced871948f987
bharath <bharathofficial133@gmail.com>
2026-02-26 17:52:05 +0530

    Update Number.java

--- Number.java
+++ Number.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class Number {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); // create Scanner inside main
+        Scanner sc = new Scanner(System.in); 
 
         System.out.println("enter a value");
         int a = sc.nextInt();
